45ae4aa8dd99fa8dd537ca5c0be2260c34b626dc
JIY00N2 <angella990825@gmail.com>
2024-06-17 16:41:02 +0900

    feat: 공통 화살표모양 함수 구현

---
+++ src/lib/utils/getArrowShape.ts
@@ -0,0 +1,40 @@
+export type ArrowDirectionType = 'top' | 'left' | 'right' | 'bottom';
+type GetArrowShape = (
+  direction: ArrowDirectionType,
+  offset: number,
+  arrowColor: string
+) => {
+  borderTop?: string;
+  borderLeft?: string;
+  borderRight?: string;
+  borderBottom?: string;
+};
+
+export const getArrowShape: GetArrowShape = (direction, offset, arrowColor) => {
+  switch (direction) {
+    case 'top':
+      return {
+        borderTop: `${offset}px solid ${arrowColor}`,
+        borderLeft: `${offset}px solid transparent`,
+        borderRight: `${offset}px solid transparent`,
+      };
+    case 'left':
+      return {
+        borderTop: `${offset}px solid transparent`,
+        borderLeft: `${offset}px solid ${arrowColor}`,
+        borderBottom: `${offset}px solid transparent`,
+      };
+    case 'right':
+      return {
+        borderTop: `${offset}px solid transparent`,
+        borderRight: `${offset}px solid ${arrowColor}`,
+        borderBottom: `${offset}px solid transparent`,
+      };
+    case 'bottom':
+      return {
+        borderLeft: `${offset}px solid transparent`,
+        borderRight: `${offset}px solid transparent`,
+        borderBottom: `${offset}px solid ${arrowColor}`,
+      };
+  }
+};
