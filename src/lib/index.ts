c148bc8f0f0d1ff941708f584df7a985189de4bd
JIY00N2 <angella990825@gmail.com>
2024-06-17 16:56:12 +0900

    feat: npm 배포 설정

---
+++ src/lib/index.ts
@@ -0,0 +1,2 @@
+export { default as Modal } from './components/Modal/Modal';
+export { default as Tooltip } from './components/Tooltip/Tooltip';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
 
-// https://vitejs.dev/config/
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'index',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        banner: '"use client";',
+        interop: 'auto',
+      },
+    },
+  },
+  plugins: [dts()],
+});
